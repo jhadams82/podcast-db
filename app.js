d3dbbe40cc612bc4643b93f153b897c0b65046ee
Justin <jhadams82@gmail.com>
2019-08-02 22:26:08 -0500

    Update app.js
    forgot to call next() after adding headers

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
         res.header('Access-Control-Allow-Methods', 'PUT, PATCH, POST, DELETE, GET');
         return res.status(200).json({});
     }
+    next();
 });
 
 app.use('/podcasts', podcastRoutes);
